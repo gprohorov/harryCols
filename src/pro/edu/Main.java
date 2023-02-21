f51feb21f86467d188293dcf1792340c453f5769
gprohorov <george.prokhorov@external.getneteurope.com>
2023-02-21 18:42:29 +0200

    list to map

--- src/pro/edu/Main.java
+++ src/pro/edu/Main.java
@@ -4,31 +4,64 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String text = new String(Files.readAllBytes(Paths.get("/home/george/Desktop/harry.txt")));
+        LocalDateTime start = LocalDateTime.now();
+
+        String text = new String(Files.readAllBytes(Paths.get("/home/george/Desktop/bigid.txt")));
 
         text = text.toLowerCase().replaceAll("[^A-Za-z ]","");
         String[] array = text.split(" +");
-        List<String> list = Arrays.stream(array).collect(Collectors.toList());
+        List<String> distinct = Arrays.stream(array).distinct().collect(Collectors.toList());
+
+        ExecutorService executorService = Executors.newFixedThreadPool(50);
+        ArrayList<Future<Map.Entry<String, Integer>>> futures = new ArrayList<>();
 
         Map<String, Integer> map = new HashMap<>();
-        Integer value = 0;
-        for (int i = 0; i < array.length; i++) {
-            if(!map.containsKey(array[i])){
-                map.put(array[i], 1);
-            }else {
-                 value = map.get(array[i]);
-                 map.put(array[i], value + 1 );
-            }
-        }
+       // Map<String, Long> collect = list.stream()
+         //       .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-    //    map.entrySet().stream().limit(20).forEach(System.out::println);
+        for (String word : distinct) {
+            int[] counter = new int[1];
+            Future<Map.Entry<String, Integer>> taskSubmitted =
+            executorService.submit( () ->{
+                counter[0] = 0;
+                        for (int j = 0; j < array.length ; j++) {
+                            if (word.equals(array[j])) counter[0]++;
+                        }
+                Map.Entry<String,Integer> entry =
+                        new AbstractMap.SimpleEntry<String, Integer>(word, counter[0]); ;
+                return entry;
+                    });
+            futures.add( taskSubmitted);
+        }
+        map = futures.stream().map(future -> {
+            try {
+                return future.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            return new AbstractMap.SimpleEntry<String, Integer>("",0);
+        }).collect(Collectors.toMap(el -> el.getKey(), el -> el.getValue()));
+
+        executorService.shutdown();
+        System.out.println(ChronoUnit.MILLIS.between(start,LocalDateTime.now())/1000.0  + " s");
+        System.out.println(map.size());
+        //    map.entrySet().stream().limit(20).forEach(System.out::println);
         Map<String, Integer> sorted = new LinkedHashMap<>();
 
 
@@ -38,6 +71,8 @@ public class Main {
 
         String sorted20 = "----------------------------" + "\n"
                 + " HEAD for the task 1" + "\n"
+                + "\n"
+                + LocalDateTime.now().toString() + "\n"
                 + "-----------------------------------" + "\n";
         int counter = 0;
         for (Map.Entry<String,Integer> entry : sorted.entrySet()){
@@ -45,9 +80,12 @@ public class Main {
             sorted20 += entry.getKey() + " " + entry.getValue() + "\n";
             if (counter == 20) break;
         }
-        String sortedToString = sorted.toString();
-        Files.write(Paths.get("/home/george/Desktop/exam.txt"),
-                sorted20.getBytes(StandardCharsets.UTF_8));
+        System.out.println(sorted20);
+        System.out.println(ChronoUnit.MILLIS.between(start,LocalDateTime.now())/1000.0  + " s");
+//        String sortedToString = sorted.toString();
+//        Files.write(Paths.get("/home/george/Desktop/exam.txt"),
+//                sorted20.getBytes(StandardCharsets.UTF_8));
+//
 
 
     }
